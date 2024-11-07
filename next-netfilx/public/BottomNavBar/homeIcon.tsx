fcb2d1f8f5c6f085a4abce6c40ac39ae752a5161
지민재 <jim400934@gmail.com>
2024-11-07 22:37:34 +0900

    Feat: BottomNavBar 공통 컴포넌트에서 쓰일 아이콘들 컴포넌트화

---
+++ next-netfilx/public/BottomNavBar/homeIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { IconProps } from "./IconPropsDto";
+
+const homeIcon: React.FC<IconProps> = ({ color = "white" }) => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M22 9.15789L12 2" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M2 9.15789L12 2" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M5 19V7.36841" stroke={color} strokeWidth="2" strokeLinecap="round"/>
+        <path d="M19 19V7.36841" stroke={color} strokeWidth="2" strokeLinecap="round"/>
+        <path d="M5 19H19" stroke={color} strokeWidth="2" strokeLinecap="round"/>
+    </svg>
+);
+
+export default homeIcon;
